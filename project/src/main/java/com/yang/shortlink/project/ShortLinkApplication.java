ad5674b7ef4e26b2673578af53f2602b5e47c07a
YangTing <2428610923@qq.com>
2026-02-01 23:16:44 +0800

    feature: 复制admin模块的基础代码至project模块补充

---
+++ project/src/main/java/com/yang/shortlink/project/ShortLinkApplication.java
@@ -0,0 +1,20 @@
+package com.yang.shortlink.project;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 启动类
+ * @author YangTing
+ * @since 2026/1/18 22:46
+ */
+@SpringBootApplication
+@MapperScan("com.yang.shortlink.project.dao.mapper")
+public class ShortLinkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShortLinkApplication.class, args);
+    }
+
+}
